0533f29652ff002d596db5b75340fa2f5452b119
Matthew Goodwin <matthewgoodwin@Matthews-MacBook-Pro-4.local>
2017-10-08 12:19:01 -0500

    Tested and corrected with solution files

--- presetHandler.js
+++ presetHandler.js
@@ -1,17 +1,30 @@
 // Use this presets array inside your presetHandler
 const presets = require('./presets');
 
-// Complete this function:
-const presetHandler = (requestType, index, newPresetArray) => {
-  if (!index) return [404]; // Invalid index
-  if (index) {
-    if (requestType === 'GET') {
-      return [200, presets];
-    } else if (requestType === 'POST') {
-        presets[i] = newPresetArray;
-        return [200, presets];
-      } else return [400]; // Bad request
+const getPreset = (index) => {
+  return presets[index] || null;
+};
+
+const createOrUpdatePreset = (index, array) => {
+  if (!presets[index]) {
+    return;
   }
+  presets[index] = array;
+  return presets[index];
+};
+
+
+// Complete this function:
+const presetHandler = (method, index, newPresetArray) => {
+  if (method === 'GET') {
+    preset = getPreset(index);
+    if (preset) return [200, preset];
+      else return [404];
+  } else if (method === 'PUT') {
+    newPreset = createOrUpdatePreset(index, newPresetArray);
+    if (newPreset) return [200, newPreset];
+      else return [404];
+  } else return [400];
 };
 
 // Leave this line so that your presetHandler function can be used elsewhere:
--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,59 @@
+const BEATS = 16;
+const createEmptyDrumArray = () => new Array(BEATS).fill(false);
+
 // Drum sounds
-let  kicks = new Array(16).fill(false);
-let  snares = new Array(16).fill(false);
-let  hiHats = new Array(16).fill(false);
-let  rideCymbals = new Array(16).fill(false);
+let  kicks = createEmptyDrumArray();
+let  snares = createEmptyDrumArray();
+let  hiHats = createEmptyDrumArray();
+let  rideCymbals = createEmptyDrumArray();
+
+const getDrumArrayByName = (name) => {
+  switch (name) {
+    case 'kicks':
+      return kicks;
+    case 'snares':
+      return snares;
+    case 'hiHats':
+      return hiHats;
+    case 'rideCymbals':
+      return rideCymbals;
+    default:
+      return;
+  }
+};
 
 function toggleDrum(sound, i) {
-  sound = window[sound];
+  sound = getDrumArrayByName(sound);
+  if (!sound || i > BEATS || i < 0) return;
   sound[i] = !sound[i];
 }
 
 function clear(sound) {
-  sound = window[sound];
-  sound.fill(false);
+  sound = getDrumArrayByName(sound);
+  if (sound) sound.fill(false);
 }
 
 function invert(sound) {
-  sound = window[sound];
-  for (let i = 0; i < sound.length; i++) {
-    sound[i] = !sound[i];
+  sound = getDrumArrayByName(sound);
+  if (sound) {
+    for (let i = 0; i < sound.length; i++) {
+      sound[i] = !sound[i];
+    }
   }
 }
 
 function getNeighborPads(x, y, size) {
-  neighbors = [];
+  neighborPads = [];
 
-  for (var row = Math.max(0, x-1); row <= Math.min(x+1, size); row++) {
-    for (var col = Math.max(0, y-1); col <= Math.min(y+1, size); col++) {
-      if (row !== x || col !== y) {
-        neighbors.push([row, col]);
-      }
-    }
-  }
+  if (x >= size || y >= size || x < 0 || y < 0) return neighborPads;
+
+  neighborPads.push([x - 1, y]);
+  neighborPads.push([x, y - 1]);
+  neighborPads.push([x + 1, y]);
+  neighborPads.push([x, y + 1]);
+  return neighborPads.filter((neighbor) => {
+    return neighbor.every((val) => {
+      return val >= 0 && val < size;
+    });
+  })
 }
